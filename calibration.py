954618612744b2c55c87db36662724c19cddd363
Irina <lazy.dare.devil@gmail.com>
2019-11-08 11:01:27 +0300

    some changes

--- calibration.py
+++ calibration.py
@@ -12,13 +12,13 @@ if not video_capture.isOpened():
     raise Exception("Could not open video device")
 
 # prepare object points
-nx = 9  # number of inside corners in x
-ny = 6  # number of inside corners in y
+nx = 9          # number of inside corners in x
+ny = 6          # number of inside corners in y
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(nx,ny,0)
 objp = np.zeros((nx*ny, 3), np.float32)
 objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
 
@@ -26,31 +26,40 @@ objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
 objpoints = []      # 3d point in real world space
 imgpoints = []      # 2d points in image plane.
 
-count = 0
-ret2 = False
+# включаем камеру, ищем шахматы
+count = 0           # счетчик сохраненных кадров
+ret2 = False        # булевая переменная, сигнализирующая о том, что были увидены шахматы
 while True:
+    # закрыть окно камеры по нажатию клавиши 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     ret1, frame = video_capture.read()
-    img = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
+    img = frame.copy()
+    ret2, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+    # если была найдена шахматка, можем сохранять кадры по нажатию клавиши 'f'
     if ret2:
+        # отображать углы найденных шахматок
+        corners2 = cv2.cornerSubPix(gray, corners, (19, 19), (-1, -1), criteria)
+        img = cv2.drawChessboardCorners(frame, (nx, ny), corners2, ret2)
         if cv2.waitKey(1) & 0xFF == ord('f'):
             cv2.imwrite("frame" + str(count) + ".jpg", frame)
             count += 1
             print('taken frame')
+    # если шахматка не найдена, продолжаем искать
     else:
         if cv2.waitKey(1) & 0xFF == ord('f'):
             print("i don't see chessboard")
-        ret2, corners = cv2.findChessboardCorners(img, (nx, ny), None)
-
+    # вычисление размытости и вывод
     fm = cv2.Laplacian(frame, cv2.CV_64F).var()
-    cv2.putText(frame, "{:12.4f}".format(fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (150, 150, 150), lineType=cv2.LINE_AA)
-    cv2.imshow('frame', frame)
+    cv2.putText(img, "{:12.4f}".format(fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (150, 150, 150), lineType=cv2.LINE_AA)
+    cv2.imshow('frame', img)
 
+# имена всех взятых кадров
 images = glob.glob('*.jpg')
 
+# пробегаем по кадрам, вычисляем object points (в трехмерном пространстве) и image points (в пространстве изображения)
 print('started watch images')
-count = 0
 for fname in images:
     print("next image")
     img = cv2.imread(fname)
@@ -63,46 +72,52 @@ for fname in images:
         objpoints.append(objp)
         corners2 = cv2.cornerSubPix(gray, corners, (19, 19), (-1, -1), criteria)
         imgpoints.append(corners2)
+        # отображение углов шахматки
         # img = cv2.drawChessboardCorners(img, (nx, ny), corners2, ret)
         # cv2.imshow('img', img)
 
 print('i had saw all images')
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+# вычисление mtx - матрица преобразования камеры и  dist - вектор коэффициентов искажения
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, cv2.cvtColor(cv2.imread(images[0]), cv2.COLOR_BGR2GRAY).shape[::-1], None, None)
 
 # writing coef into coef.txt
-# (описание коэффициентов - добавить - не будь Тимуром)
 file = open("coef.txt", "w+")
-strr = "3\n3\n"
+# размерность матрицы - первые два числа
+strr = str(len(mtx)) + "\n" + str(len(mtx[0])) + "\n"
+# потом сама матрица соответствующей размерности
 for i in range(len(mtx[0])):
     for j in range(len(mtx)):
         strr += str(mtx[i, j]) + "\n"
+# размер вектора коэффициентов искажения
 strr += str(len(dist[0])) + "\n"
+# сам вектор искажения
 for i in range(len(dist[0])):
     strr += str(dist[0, i]) + "\n"
 file.write(strr)
 file.close()
 
-h, w = img.shape[:2]
+h, w = cv2.imread(images[0]).shape[:2]
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+x, y, w, h = roi
 
-count = 0
+# сохраняем изображения - уже с откалиброванной камерой
+count = 0       # счетчик изображений
 for fname in images:
+    # undistortion
     dst = cv2.undistort(cv2.imread(fname), mtx, dist, None, newcameramtx)
-    x, y, w, h = roi
+    # crop the image
     dst = dst[y:y + h, x:x + w]
-    cv2.imwrite("calib" + str(count) + ".jpg", dst)
+    cv2.imwrite("calib" + str(count) + ".png", dst)
     count += 1
 
+# запускаем камеру с применением калибровки
 while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     ret, frame = video_capture.read()
     # undistortion
     dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
-    # mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
-    # dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
     # crop the image
-    x, y, w, h = roi
     dst = dst[y:y + h, x:x + w]
     cv2.imshow('frame', dst)
 
